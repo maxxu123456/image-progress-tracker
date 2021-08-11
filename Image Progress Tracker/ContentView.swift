407ce8e4cdf11baf04b2576b22196527cf4525a5
maxxu123456 <62445552+maxxu123456@users.noreply.github.com>
2021-08-10 23:28:11 -0400

    Initial Commit

---
+++ Image Progress Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Image Progress Tracker
+//
+//  Created by Max Xu on 8/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Image Progress Tracker/Image_Progress_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_Progress_TrackerApp.swift
+//  Image Progress Tracker
+//
+//  Created by Max Xu on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Image_Progress_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
